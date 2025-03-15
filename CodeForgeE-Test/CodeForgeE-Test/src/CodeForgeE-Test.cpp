36ebfa7f4e60f90113815f83a6a71d82824a018b
TSPLuchev <TSPLuchev23@codingburgas.bg>
2025-03-16 00:16:16 +0200

    Update CodeForgeE-Test.cpp

--- CodeForgeE-Test/CodeForgeE-Test/src/CodeForgeE-Test.cpp
+++ CodeForgeE-Test/CodeForgeE-Test/src/CodeForgeE-Test.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include <string>
-#include "raylib.h"
-#include "questions.h"
-#include "results.h"
-#include "test.h"
-#include "raylibRunner.h"
-using namespace std;
-
-int main() {
-    RunRaylib();
-    return 0;
-}
 
